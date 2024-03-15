f366d98b57b79b869cca4d9777e8816a75db3c17
Raj Aryan Dash <rajaryanrad@gmail.com>
2024-03-15 17:25:41 +0530

    Added result page and connected flask to website

--- .venv/frontend/app.py
+++ .venv/frontend/app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return render_template("index.html")
+    return render_template("index.html", home=True)
+
+@app.route('/face_check', methods=['post'])
+def face_check():
+    opt = ''
+
+    if 'recognise' in request.form:
+        opt = 'recognise'
+    elif 'saveFace' in request.form:
+        opt = 'saveFace'
+
+    return render_template('result.html', result=opt)
 
 
 if __name__ == "__main__":
