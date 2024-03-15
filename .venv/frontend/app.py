f42c6c350073d97725f49a1cd7c7b6eef116d627
Raj Aryan Dash <rajaryanrad@gmail.com>
2024-03-15 17:52:40 +0530

    Commented everything so far

--- .venv/frontend/app.py
+++ .venv/frontend/app.py
@@ -1,22 +1,25 @@
 from flask import Flask, render_template, request, url_for
 
+
 app = Flask(__name__)
 
 
+# This runs the home page
 @app.route('/')
 def hello():
-    return render_template("index.html", home=True)
+    return render_template("index.html", home=True) # Renders the homepage and returns for the browser to load
 
+# When the form is submitted using one of the two buttons on the website, this function is triggered
 @app.route('/face_check', methods=['post'])
 def face_check():
-    opt = ''
+    result = '' 
 
     if 'recognise' in request.form:
-        opt = 'recognise'
+        result = 'recognise'
     elif 'saveFace' in request.form:
-        opt = 'saveFace'
+        result = 'saveFace'
 
-    return render_template('result.html', result=opt)
+    return render_template('result.html', result=result) # Renders the result page and shows whether the face is recognised or not
 
 
 if __name__ == "__main__":
